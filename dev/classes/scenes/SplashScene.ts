39c2bd0bb62b1e967835930f2aa401265898759c
SPMTD <sanderr97@live.nl>
2016-06-13 11:38:35 +0200

    Added second playable character.

--- dev/classes/scenes/SplashScene.ts
+++ dev/classes/scenes/SplashScene.ts
@@ -9,6 +9,7 @@ class SplashScene extends Scene
         this.gameObjects.push(new FadeText(new Vector2(Game.width / 2 - 50, Game.height / 2), 100 , 50, "Welkom!", 24, 0, 100, 0, 0.25, 1.0, 0.5));
         this.gameObjects.push(new Puss(new Vector2(0,0), 50, 50, 0.75));
         //this.gameObjects.push(new Wall(new Vector2(600, Game.height / 2), 64, 256, false, true));
+        this.gameObjects.push(new Knightsalot(new Vector2(0,0), 50, 50, 0.75));
         
         // Load level.
         this.level = new LevelLoader();       
